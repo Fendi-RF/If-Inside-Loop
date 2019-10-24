849f466f1b8a7f2e30839188c343385636518621
fendiridho <fendiridho@gmail.com>
2019-10-24 19:56:07 +0700

    if inside loop dengan c#

---
+++ If-inside-loop/If-inside-loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_inside_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 20; a++)
+            {
+                Console.WriteLine("Umur saya " + a + " tahun.");
+                if (a == 17)
+                {
+                    Console.WriteLine("Umur saya " + a + " tahun. Saya sudah dewasa");
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
